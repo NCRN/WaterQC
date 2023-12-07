76aeb0d367fffb23396bbb8157a78cbadb1543b5
Danmyers901 <71978159+Danmyers901@users.noreply.github.com>
2023-12-07 06:47:36 -0500

    Update Water QC - Populating constraints tables.R
    Added by-month

--- Water QC - Populating constraints tables.R
+++ Water QC - Populating constraints tables.R
@@ -6,10 +6,11 @@
 # the blank SoftConstraints.csv, EDD, and Lookup.csv (on GitHub) as inputs.
 
 ################################################################################
-### Format data ################################################################
+### Format data (by site) ######################################################
 ################################################################################
 
 # Load packages
+library(tibble)
 library(tidyr)
 library(dplyr)
 library(lubridate)
@@ -50,12 +51,12 @@ wqdata <- wdata[wdata$ProjectIdentifier=="USNPS NCRN Perennial stream water moni
 low <- wqdata %>%
   group_by(MonitoringLocationIdentifier) %>%
   group_by(CharacteristicName, .add=T) %>%
-  summarise(low=quantile(ResultMeasureValue,0.05,na.rm=T))
+  summarise(low=quantile(ResultMeasureValue,0.10,na.rm=T))
 
 high <- wqdata %>%
   group_by(MonitoringLocationIdentifier) %>%
   group_by(CharacteristicName, .add=T) %>%
-  summarise(high=quantile(ResultMeasureValue,0.95,na.rm=T))
+  summarise(high=quantile(ResultMeasureValue,0.90,na.rm=T))
 
 
 # Remove NAs and extra choice list characteristics
@@ -120,4 +121,97 @@ sites <- data.frame(Location_ID = s_con[,1])
 s_con_filled <- left_join(sites, sorted, by="Location_ID")
 
 # Write as csv
-write.csv(s_con_filled,"SoftConstraints-filled.csv",row.names=F, quote=F)
+# write.csv(s_con_filled,"SoftConstraints-filled 10-90.csv",row.names=F, quote=F)
+
+
+
+################################################################################
+### Identify low and high values (by month) ####################################
+################################################################################
+
+# Summarize sites and characteristics
+low <- wqdata %>%
+  group_by(MonitoringLocationIdentifier) %>%
+  group_by(Month, .add=T) %>%
+  group_by(CharacteristicName, .add=T) %>%
+  summarise(low=quantile(ResultMeasureValue,0.10,na.rm=T))
+
+high <- wqdata %>%
+  group_by(MonitoringLocationIdentifier) %>%
+  group_by(Month, .add=T) %>%
+  group_by(CharacteristicName, .add=T) %>%
+  summarise(high=quantile(ResultMeasureValue,0.90,na.rm=T))
+
+
+# Remove NAs and extra choice list characteristics
+low <- low[!is.na(low$low),]
+low <- low[!low$CharacteristicName %in% c(
+  "Weather Condition (WMO Code 4501) (choice list)",
+  "RBP2, Low G, Riparian Vegetative Zone Width, Left Bank (choice list)",
+  "RBP2, Low G, Riparian Vegetative Zone Width, Right Bank (choice list)"),]
+
+high <- high[!is.na(high$high),]
+high <- high[!high$CharacteristicName %in% c(
+  "Weather Condition (WMO Code 4501) (choice list)",
+  "RBP2, Low G, Riparian Vegetative Zone Width, Left Bank (choice list)",
+  "RBP2, Low G, Riparian Vegetative Zone Width, Right Bank (choice list)"),]
+
+pw_low <- low %>%
+  pivot_wider(names_from="CharacteristicName", values_from="low")
+
+pw_high <- high %>%
+  pivot_wider(names_from="CharacteristicName", values_from="high")
+
+
+################################################################################
+### Match columns (by month) ###################################################
+################################################################################
+
+# Extract column names
+low_cn <- data.frame(CharacteristicName = colnames(pw_low))
+high_cn <- data.frame(CharacteristicName = colnames(pw_high))
+
+# Lookup constraint names
+low_cn <- left_join (low_cn, lookup[lookup$Type=="low",], by="CharacteristicName")
+high_cn <- left_join (high_cn, lookup[lookup$Type=="high",], by="CharacteristicName")
+
+# Name fields
+low_cn$SoftConstraints_name[1] <- "MonitoringLocationIdentifier"
+low_cn$SoftConstraints_name[2] <- "Month"
+high_cn$SoftConstraints_name[1] <- "MonitoringLocationIdentifier"
+high_cn$SoftConstraints_name[2] <- "Month"
+
+# Rename columns
+colnames(pw_low) <- low_cn$SoftConstraints_name
+colnames(pw_high) <- high_cn$SoftConstraints_name
+
+# Remove NA columns
+pw_low <- pw_low[,!is.na(colnames(pw_low))]
+pw_high <- pw_high[,!is.na(colnames(pw_high))]
+
+# Join together
+lowHigh <- left_join(pw_low, pw_high, by=c("MonitoringLocationIdentifier","Month"))
+colnames(lowHigh)[1] <- "Location_ID"
+
+# Add turbidity columns (if needed)
+if (!"turbidity_soft_constraint_low" %in% colnames(lowHigh)){
+  lowHigh[,length(lowHigh)+1] <- NA
+  colnames(lowHigh)[length(lowHigh)] <- "turbidity_soft_constraint_low"
+  lowHigh[,length(lowHigh)+1] <- NA
+  colnames(lowHigh)[length(lowHigh)] <- "turbidity_soft_constraint_high"
+}
+
+# Add month column to s_con
+s_con2 <- add_column(s_con, Month=NA,.before=2)
+
+# Sort columns
+sorted <- lowHigh[,colnames(s_con2)]
+
+# Fill soft constraints table
+sites <- data.frame(Location_ID = s_con[,1])
+sites_mon <- data.frame(Location_ID = rep(sites$Location_ID,each=12,times=nrow(sites)),
+                        Month = rep(1:12,times=nrow(sites)))
+s_con_filled <- left_join(sites_mon, sorted, by=c("Location_ID","Month"))
+
+# Write as csv
+# write.csv(s_con_filled,"SoftConstraints-filled by month 10-90.csv",row.names=F, quote=F)
